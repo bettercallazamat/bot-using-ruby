5f4235f353a58039ee696a83243b77c8493ced34
bettercallazamat <bettercallazamat@gmail.com>
2020-04-10 04:09:53 +0600

    Change token

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,13 @@
 # coding: UTF-8
 require "telegram/bot"
 
-TOKEN = '1256706071:AAHtbdLD8bbzk61b6gwKBdBDXAygkLErUcw'
+TOKEN = '1256706071:AAE_fzzEcpI0Y-GSDmAqO11mleVHxDOApuA'
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Welcome, #{message.from.first_name}.")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
       exit
