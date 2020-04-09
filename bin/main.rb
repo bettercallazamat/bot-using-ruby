9f1b72a293175757fe97141e7bf13395c08ec811
bettercallazamat <bettercallazamat@gmail.com>
2020-04-10 01:35:03 +0600

    Create basic telegram bot structure

---
+++ bin/main.rb
@@ -0,0 +1,14 @@
+require "telegram/bot"
+
+TOKEN = '1256706071:AAHtbdLD8bbzk61b6gwKBdBDXAygkLErUcw'
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
