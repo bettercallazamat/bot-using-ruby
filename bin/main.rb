399d9ba91a79c2995a55ff2ff2853364bc095ce1
bettercallazamat <bettercallazamat@gmail.com>
2020-04-10 04:14:59 +0600

    Add auth reqest text

--- bin/main.rb
+++ bin/main.rb
@@ -7,7 +7,7 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Welcome, #{message.from.first_name}.")
+      bot.api.send_message(chat_id: message.chat.id, text: "Welcome, #{message.from.first_name}. Please authorize bot via OAuth, so I can help you.\n Type /auth and provide me your github account")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
       exit
