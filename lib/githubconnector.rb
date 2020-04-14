a868eda18b55aeb0a4b2aeecbe8ce2a5c6728874
bettercallazamat <bettercallazamat@gmail.com>
2020-04-15 04:10:32 +0600

    Remove basic_auth because of heroku problem

--- lib/githubconnector.rb
+++ lib/githubconnector.rb
@@ -12,26 +12,26 @@ class GitHubConnector
   base_uri 'https://api.github.com'
 
   def repos(username)
-    obj = self.class.get("/users/#{username}/repos", ENV['BASIC_AUTH'])
+    obj = self.class.get("/users/#{username}/repos")
     repos_array = []
     obj.each { |value| repos_array.push(value['name']) }
     repos_array
   end
 
   def pull_requests(username, repo)
-    obj = self.class.get("/repos/#{username}/#{repo}/pulls", ENV['BASIC_AUTH'])
+    obj = self.class.get("/repos/#{username}/#{repo}/pulls")
     pull_requests_array = []
     obj.each { |value| pull_requests_array.push(value['title']) }
     pull_requests_array
   end
 
   def comments_num(username, repo, num)
-    obj = self.class.get("/repos/#{username}/#{repo}/issues/#{num}/comments", ENV['BASIC_AUTH'])
+    obj = self.class.get("/repos/#{username}/#{repo}/issues/#{num}/comments")
     obj.length
   end
 
   def pr_exists?(username, repo, num)
-    obj = self.class.get("/repos/#{username}/#{repo}/issues/#{num}/comments", ENV['BASIC_AUTH'])
+    obj = self.class.get("/repos/#{username}/#{repo}/issues/#{num}/comments")
     if obj[0].nil?
       false
     else
