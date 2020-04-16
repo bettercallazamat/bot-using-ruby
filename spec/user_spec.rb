28e2b9fb62cbde24d630966dfb8b25b00b9d66d7
bettercallazamat <bettercallazamat@gmail.com>
2020-04-16 23:24:54 +0600

    Fix rspec tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,17 +1,17 @@
-# # spec :user_spec.rb
-# # rubocop:disable Layout/LineLength
+# spec :user_spec.rb
+# rubocop:disable Layout/LineLength
 
-# require './lib/user.rb'
+require './lib/user.rb'
 
-# RSpec.describe(User) do
-#   let(:user) { User.new('bettercallazamat') }
-#   describe '.initialize' do
-#     context 'when bot runs this method' do
-#       it 'creates instance of User class' do
-#         expect(user.class).to eq User
-#       end
-#     end
-#   end
-# end
+RSpec.describe(User) do
+  let(:user) { User.new('bettercallazamat') }
+  describe '.initialize' do
+    context 'when bot runs this method' do
+      it 'creates instance of User class' do
+        expect(user.class).to eq User
+      end
+    end
+  end
+end
 
-# # rubocop:enable Layout/LineLength
+# rubocop:enable Layout/LineLength
